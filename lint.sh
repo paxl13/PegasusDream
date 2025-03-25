abadfd6234f90c55fef7d1a696d26e02481adffc
Xavier LaRue <paxl13@gmail.com>
2025-03-25 14:10:12 -0400

    Better linter / compilation process

--- lint.sh
+++ lint.sh
@@ -1,1 +1,11 @@
-find src/ | entr -s 'clear; shrinko8 -l src/pegasus.p8 --no-lint-unused-global --count'
+mkdir -p dist/
+find src/ | entr -s ' \
+  clear; \
+  shrinko8 \
+    --lint \
+    --no-lint-unused-global \
+    --count --input-count \
+    -m --rename-safe-only --focus-token \
+    src/pegasus.p8 \
+    dist/pegasus.p8.png \
+'
